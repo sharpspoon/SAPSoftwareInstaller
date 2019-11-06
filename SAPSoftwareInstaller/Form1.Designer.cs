cbc8eda19424432329387949226d114a8ce0d7f5
Robin Ward <robin@steelcitysites.net>
2019-11-06 15:10:38 -0600

    init

--- SAPSoftwareInstaller/Form1.Designer.cs
+++ SAPSoftwareInstaller/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SAPSoftwareInstaller
 {
-    partial class Form1
+    partial class SAPSoftwareInstallerForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SAPSoftwareInstallerForm));
+            this.Install_ICM_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Install_ICM_Button
+            // 
+            this.Install_ICM_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Install_ICM_Button.Location = new System.Drawing.Point(12, 12);
+            this.Install_ICM_Button.Name = "Install_ICM_Button";
+            this.Install_ICM_Button.Size = new System.Drawing.Size(163, 46);
+            this.Install_ICM_Button.TabIndex = 0;
+            this.Install_ICM_Button.Text = "Install ICM";
+            this.Install_ICM_Button.UseVisualStyleBackColor = true;
+            // 
+            // SAPSoftwareInstallerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.Install_ICM_Button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SAPSoftwareInstallerForm";
+            this.Text = "SAP Software Installer";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Install_ICM_Button;
     }
 }
 
--- SAPSoftwareInstaller/Program.cs
+++ SAPSoftwareInstaller/Program.cs
@@ -16,7 +16,7 @@ namespace SAPSoftwareInstaller
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SAPSoftwareInstallerForm());
         }
     }
 }
